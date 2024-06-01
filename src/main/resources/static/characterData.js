809a9bab5519c050246dfe4f8653bb467ca408ae
cameron-leung <cameronleung@berkeley.edu>
2024-06-01 16:39:18 -0700

    abstract -> characterData.js, circle, circles, card, & cards components

---
+++ src/main/resources/static/characterData.js
@@ -0,0 +1,8 @@
+const characterData = {
+    name: 'Lili',
+    circimageUrl: 'images/lilicircle.png',
+    rectimageUrl: 'images/LiliRectangle.png',
+    username: '@B',
+    price: '$2.00',
+    description: 'Your guide to small talk'
+};
