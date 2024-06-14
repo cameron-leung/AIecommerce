c062726cf2b8810484de9de64f87905884143ec1
cameron-leung <cameronleung@berkeley.edu>
2024-06-14 13:35:12 -0700

    chattercards for loop wip

--- src/main/resources/static/characterData.js
+++ src/main/resources/static/characterData.js
@@ -1,8 +1,10 @@
 const characterData = {
     name: 'Lili',
+    category: 'Helpful',
     circimageUrl: 'images/lilicircle.png',
     rectimageUrl: 'images/LiliRectangle.png',
     username: '@B',
     price: '$2.00',
-    description: 'Your guide to small talk'
+    blurb: 'Your guide to small talk',
+    description: 'Young woman with modern, open-minded, charismatic personality. Expert in social skills and first impressions. Role-plays educational mock interactions over call and video call.'
 };
