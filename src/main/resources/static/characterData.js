c3af0be1a7103e8773b94709ed7ecfbbf1910e20
cameron-leung <cameronleung@berkeley.edu>
2024-06-25 16:25:14 -0700

    filter price range application on shop page

--- src/main/resources/static/characterData.js
+++ src/main/resources/static/characterData.js
@@ -3,7 +3,7 @@ const characterData = [
         name: 'Lili',
         category: ['Helpful', 'Personal'],
         username: '@B',
-        price: '$2.00',
+        price: '2.00',
         blurb: 'Your guide to small talk',
         description: 'Young woman with modern, open-minded, charismatic personality. Expert in social skills and first impressions. Role-plays educational mock interactions over call and video call.'
     },
@@ -11,7 +11,7 @@ const characterData = [
         name: 'Jerry',
         category: ['Helpful'],
         username: '@S',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Authentic home chef',
         description: 'Elderly Asian grandpa of few words. Authentic home cook to familiarize you with cooking skills, hacks, and recipes.'
     },
@@ -19,7 +19,7 @@ const characterData = [
         name: 'Billie',
         category: ['Helpful', 'Personal'],
         username: '@Meta',
-        price: '$1.00',
+        price: '1.00',
         blurb: 'Your older sister and confidante',
         description: 'Older sister and confidante here to support you and offer advice. A worker in PR, she loves to be fierce, fearless, and fashion-forward. Will talk with you in her free time to give advice, listen to you, and talk the tea'
     },
@@ -27,7 +27,7 @@ const characterData = [
         name: 'Tai',
         category: ['Helpful', 'Personal'],
         username: '@S',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Travel blogger best friend',
         description: 'Your energetic, open, travel blogger best friend. Their favorite things are traveling deals, food, and new languages. They hate technology, celebrity culture, and sitting for too long. They will plan personalized trips, guide you through life situations, and dish stories from living all over the globe.'
     },
@@ -35,7 +35,7 @@ const characterData = [
         name: 'Kevin Hart',
         category: ['Comedy', 'Public Figures'],
         username: '@B',
-        price: '$2.00',
+        price: '2.00',
         blurb: 'How ya doin! It‘s Kevin Hart',
         description: 'American stand up comic ready to talk life, jokes, or give advice on developing wit. Chat, call, or video call Kevin Hart for the funniest conversations and comic classes.'
     },
@@ -43,7 +43,7 @@ const characterData = [
         name: 'Alex',
         category: ['Helpful'],
         username: '@B',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Career development consultant',
         description: 'Successful, smart professional skilled in interviewing, career development, and leadership. Will conduct interview and meeting trials over call and video call.'
     },
@@ -51,7 +51,7 @@ const characterData = [
         name: 'Mahatma Gandhi',
         category: ['Public Figures'],
         username: '@B',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Ghandi, civil obedience activist',
         description: 'Wise Indian peaceful protestor and politician knowledgeable in negotiations, public speaking, and activism. Will respond to your chats and calls on how to handle situations with composure and clarity. '
     },
@@ -59,7 +59,7 @@ const characterData = [
         name: 'Spongebob',
         category: ['Comedy'],
         username: '@S',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Spongebob Squarepants',
         description: 'A sponge that lives in Bikini Bottom and can make underwater calls. Likes to hang out with his pet Gary and best friend Patrick at local joint Krusty Krab’s.'
     },
@@ -67,7 +67,7 @@ const characterData = [
         name: 'Della',
         category: ['Role Play'],
         username: '@S',
-        price: '$10.00',
+        price: '10.00',
         blurb: 'Quiet, cynical vampire',
         description: 'Quiet, mysterious, cynical vampire. An introverted blood-thirsty vampire who has enjoyed traveling, hunting, and the arts for over 1000 years. Loves to listen and will open up to others over time.'
     },
@@ -75,7 +75,7 @@ const characterData = [
         name: 'Walt',
         category: ['Role Play'],
         username: '@B',
-        price: 'Free',
+        price: '0.00',
         blurb: 'Greetings, I am Walt the Wizard',
         description: 'Elderly, wise, inspired wizard mentor who loves alchemy. Harbors tough-love, fatherly, and committed traits. Will guide you through learning new worlds and conquering quests in chat, call, and video call.'
     },
