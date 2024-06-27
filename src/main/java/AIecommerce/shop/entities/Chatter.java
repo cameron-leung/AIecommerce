91d8dcc6542bad8c00226aee9cbd28699d255248
cameron-leung <cameronleung@berkeley.edu>
2024-06-27 07:12:18 -0700

    chatter java class

---
+++ src/main/java/AIecommerce/shop/entities/Chatter.java
@@ -0,0 +1,76 @@
+package AIecommerce.shop.entities;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Chatter {
+	private String name;
+	private List<String> category;
+	private String username;
+	private int price;
+	private String blurb;
+	private String description;
+	
+	// Default constructor
+    public Chatter() {
+        this.category = new ArrayList<>();
+    }
+
+    // Parameterized constructor
+    public Chatter(String name, List<String> category, String username, int price, String blurb, String description) {
+        this.name = name;
+        this.category = category;
+        this.username = username;
+        this.price = price;
+        this.blurb = blurb;
+        this.description = description;
+    }
+
+    // Getter and setter methods
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getCategory() {
+        return category;
+    }
+
+    public void setCategory(List<String> category) {
+        this.category = category;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public void setBlurb(String blurb) {
+        this.blurb = blurb;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
