b7b45b9108cb8508c5151a107ff1a4fe4e77e222
cameron-leung <cameronleung@berkeley.edu>
2024-07-09 16:36:25 -0400

    connecting frontend & backend profile making

--- src/main/java/store/aiexchange/shop/rest/ProfileRest.java
+++ src/main/java/store/aiexchange/shop/rest/ProfileRest.java
@@ -1,16 +1,14 @@
 package store.aiexchange.shop.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import store.aiexchange.shop.entities.Chatter;
-import store.aiexchange.shop.entities.Order;
 import store.aiexchange.shop.entities.Profile;
 import store.aiexchange.shop.entities.ProfileRepository;
 
@@ -21,10 +19,15 @@ public class ProfileRest {
     private ProfileRepository profileRepository;
 
     @PostMapping("/createAccount")
-    public Profile createProfile(@RequestBody Profile accountData) {
-    	Profile newProfile = new Profile(accountData.getName(), accountData.getUsername(),
+    public ResponseEntity<?> createProfile(@RequestBody Profile accountData) {
+        Profile existingProfile = profileRepository.findByUsername(accountData.getUsername());
+        if (existingProfile != null) {
+            return new ResponseEntity<>("Username already exists", HttpStatus.CONFLICT);
+        }
+        Profile newProfile = new Profile(accountData.getName(), accountData.getUsername(),
                 accountData.getEmail(), accountData.getPassword());
-    	return profileRepository.save(newProfile);
+        profileRepository.save(newProfile);
+        return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
     }
 
     @GetMapping("/findByName")
