375907bebff3a274bce5419b73d8b426ca744cb9
cameron-leung <cameronleung@berkeley.edu>
2024-07-22 21:48:18 -0400

    chatter preview .js

---
+++ src/main/resources/static/assets/js/chatterpreview.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	// Load navbar & activate discover icon
+	$('#navbar-placeholder').load('navbar.html', function() {
+		$('.discover-icon').addClass('active');
+	});
+	// Load details
+	const urlParams = new URLSearchParams(window.location.search);
+	const characterName = urlParams.get('name');
+	loadCharacterDetails(characterName);
+	populateVerticalScroll();
+});
+function populateDetails(character) {
+	const imageName = character.name.replace(/\s+/g, '');
+	$('.chatter-image').attr('src', 'assets/img/' + imageName + 'Rectangle.jpg');
+	$('.chatter-name').text(character.name);
+	$('.chatter-username').text(character.username);
+	$('.chatter-price').text('$' + character.price.toFixed(2));
+	$('.chatter-description').text(character.description);
+}
+// Find chatter for chatter preview
+function loadCharacterDetails(characterName) {
+	if (characterName) {
+		// Fetch character details from the backend
+		$.getJSON('/chatters', function(characterData) {
+			const character = characterData.find(c => c.name === characterName);
+			if (!character) {
+				console.error(`Character with name ${characterName} not found.`);
+				return;
+			}
+			// Update with character details
+			populateDetails(character);
+			// Attach event handler to the cart icon
+			$('.fa-cart-plus').off('click').on('click', function() {
+				addToCart(character);
+				fetchCartItems();
+			});
+		});
+	}
+}
+
+function populateVerticalScroll() {
+	// Vertical scroll images
+	var imageContainer = document.getElementById('image-container');
+	var numberOfImages = 5; // Number of times to repeat the image
+	for (var i = 0; i < numberOfImages; i++) {
+		var img = document.createElement('img');
+		img.classList.add('chatter-image'); // Add chatter-image class
+		img.alt = 'Chatter Card ' + (i + 1);
+		imageContainer.appendChild(img);
+	}
+}
--- src/main/resources/static/assets/js/script.js
+++ src/main/resources/static/assets/js/script.js
@@ -50,82 +50,7 @@ function profileButton() {
 	});
 }
 
-
-// Function to fill in card data
-function populateCard(template, character) {
-	const imageName = character.name.replace(/\s+/g, '');
-	return template
-		.replace(/RECT_IMAGE_URL/g, 'assets/img/' + imageName + 'Rectangle.jpg')
-		.replace(/NAME/g, character.name)
-		.replace(/USER/g, character.username)
-		.replace(/PRICE/g, character.price.toFixed(2))
-		.replace(/BLURB/g, character.blurb);
-}
-// Function to fill in circle data
-function populateCircle(template, character) {
-	const imageName = character.name.replace(/\s+/g, '');
-	return template
-		.replace('CIRC_IMAGE_URL', 'assets/img/' + imageName + 'Circle.jpg')
-		.replace('NAME', character.name)
-}
-
-function populateChatterCircles(myChatters) {
-	const container = $('.character-circ-container');
-	//container.empty(); // Clear the container before appending new content
-
-	// Add the header to the container
-	container.append('<h1 class="mt-3 display-4">Recents</h1>');
-	// Fetch chattercard template
-	$.get('chattercircle.html', function(template) {
-		myChatters.forEach(character => {
-			const populatedCircle = populateCircle(template, character);
-			container.append(populatedCircle);
-		});
-	})
-}
-// Chatter Preview logic
-// Populate details on chatter preview
-function populateDetails(character) {
-	const imageName = character.name.replace(/\s+/g, '');
-	$('.chatter-image').attr('src', 'assets/img/' + imageName + 'Rectangle.jpg');
-	$('.chatter-name').text(character.name);
-	$('.chatter-username').text(character.username);
-	$('.chatter-price').text('$' + character.price.toFixed(2));
-	$('.chatter-description').text(character.description);
-}
-// Find chatter for chatter preview
-function loadCharacterDetails(characterName) {
-	if (characterName) {
-		// Fetch character details from the backend
-		$.getJSON('/chatters', function(characterData) {
-			const character = characterData.find(c => c.name === characterName);
-			if (!character) {
-				console.error(`Character with name ${characterName} not found.`);
-				return;
-			}
-			// Update with character details
-			populateDetails(character);
-			// Attach event handler to the cart icon
-			$('.fa-cart-plus').off('click').on('click', function() {
-				addToCart(character);
-				fetchCartItems();
-			});
-		});
-	}
-}
-function populateVerticalScroll() {
-	// Vertical scroll images
-	var imageContainer = document.getElementById('image-container');
-	var numberOfImages = 5; // Number of times to repeat the image
-	for (var i = 0; i < numberOfImages; i++) {
-		var img = document.createElement('img');
-		img.classList.add('chatter-image'); // Add chatter-image class
-		img.alt = 'Chatter Card ' + (i + 1);
-		imageContainer.appendChild(img);
-	}
-}
 // Search query logic
-
 function loadQuery() {
 	var urlParams = new URLSearchParams(window.location.search);
 	var query = urlParams.get('query');
@@ -169,4 +94,36 @@ function queryCharacterData(query) {
 	}).fail(function() {
 		$('#no-data-message').text('No Chatters found');
 	});
+}
+// Function to fill in card data
+function populateCard(template, character) {
+	const imageName = character.name.replace(/\s+/g, '');
+	return template
+		.replace(/RECT_IMAGE_URL/g, 'assets/img/' + imageName + 'Rectangle.jpg')
+		.replace(/NAME/g, character.name)
+		.replace(/USER/g, character.username)
+		.replace(/PRICE/g, character.price.toFixed(2))
+		.replace(/BLURB/g, character.blurb);
+}
+// Function to fill in circle data
+function populateCircle(template, character) {
+	const imageName = character.name.replace(/\s+/g, '');
+	return template
+		.replace('CIRC_IMAGE_URL', 'assets/img/' + imageName + 'Circle.jpg')
+		.replace('NAME', character.name)
+}
+
+function populateChatterCircles(myChatters) {
+	const container = $('.character-circ-container');
+	//container.empty(); // Clear the container before appending new content
+
+	// Add the header to the container
+	container.append('<h1 class="mt-3 display-4">Recents</h1>');
+	// Fetch chattercard template
+	$.get('chattercircle.html', function(template) {
+		myChatters.forEach(character => {
+			const populatedCircle = populateCircle(template, character);
+			container.append(populatedCircle);
+		});
+	})
 }
