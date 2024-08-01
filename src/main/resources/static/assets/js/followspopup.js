0287808a59cdd6b1dc05816eb44200c88530acc7
cameron-leung <cameronleung@berkeley.edu>
2024-08-01 19:59:06 -0400

    followspopup empty message & wip

--- src/main/resources/static/assets/js/followspopup.js
+++ src/main/resources/static/assets/js/followspopup.js
@@ -1,4 +1,12 @@
 var followsPopupOpen = false;
+function followProfileHtml(name) {
+	return `
+        <div class="d-flex flex-row">
+        	<i class="fa-solid fa-circle-user responsive-icon w-25 pr-1 pt-1" style="color: #535355;"></i>
+        	<h3 ">${name}</h3>
+        </div>
+    `;
+}
 	// Open the cart popup
 	function openFollowsPopup(followString) {
 		if (!followsPopupOpen) {
@@ -6,6 +14,18 @@ var followsPopupOpen = false;
 			$('#follows-header').text(followString);
 			followsPopupOpen = true;
 			const profile = JSON.parse(Cookies.get('profile'));
+			const profilesContainer = $('#profiles-container');
+			profilesContainer.empty();
+			if(profile.followers.length) {
+				profile.followers.forEach(function(profileId) {
+					
+					var profileHtml = followProfileHtml(profile);
+						container.append(profileHtml);
+				})
+			} else {
+				$('#no-follows-message').show();
+        		$('#no-follows-message').text('no followers');
+			}
 }
 	}
 	// Close the cart popup
